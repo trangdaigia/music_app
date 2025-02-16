2c0440a5908ac5c35e7f3e7fa839203fbe4c727b
trangdaigia <2254810019@vaa.edu.vn>
2025-02-16 22:39:47 +0700

    Gia

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+import 'package:music_app/ui/home/home.dart';
+
+void main () => runApp(const MusicApp());
---
+++ lib/ui/now_playing/playing.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../data/model/song.dart';
+
+class NowPlaying extends StatelessWidget {
+  const NowPlaying({super.key, required this.playingSong, required this.songs});
+
+  final Song playingSong;
+  final List<Song> songs;
+
+  @override
+  Widget build(BuildContext context) {
+    return NowPlayingPage(
+      songs: songs,
+      playingSong: playingSong,
+    );
+  }
+}
+
+class NowPlayingPage extends StatefulWidget {
+  const NowPlayingPage(
+      {super.key, required this.songs, required this.playingSong});
+
+  final Song playingSong;
+  final List<Song> songs;
+
+  @override
+  State<NowPlayingPage> createState() => _NowPlayingPageState();
+}
+
+class _NowPlayingPageState extends State<NowPlayingPage> {
+  @override
+  Widget build(BuildContext context) {
+    // return Scaffold(
+    //   body: Center(
+    //     child: Text('Now Playing'),
+    //   ),
+    // );
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text(
+          'Now Playing',
+        ),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.more_horiz),
+        ),
+      ),
+      child: const Center(
+        child: Text('Now Playing Page'),
+      ),
+    );
+  }//tét
+}
